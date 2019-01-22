32966f7dd5423480e69e96693b9d002ef83da5dd
arbhassan <hassanjawad1321@gmail.com>
2019-01-22 12:24:09 +0500

    REMOVED SECRET URL

--- models/index.js
+++ models/index.js
@@ -3,8 +3,6 @@ var mongoose = require('mongoose');
 var url = process.env.MONGO_URI;
 mongoose.set('debug', true);
 mongoose.connect(url, {useNewUrlParser: true});
-// mongoose.connect('mongodb://127.0.0.1:27017');
-// mongoose.connect('mongodb://arbhassan:slimrapper13@ds047666.mlab.com:47666/node-api', {useNewUrlParser: true});
 
 mongoose.Promise = Promise;
 
